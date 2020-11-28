47a39d91c6c556c481095f6983618976e59f29bb
Jez Nicholson <jez.nicholson@gmail.com>
2020-11-28 16:05:51 +0000

    neater

--- test_handler.py
+++ test_handler.py
@@ -3,14 +3,11 @@ import json
 
 
 def test_wind():
-    response = handler.hello({
-        "id": "way/222920036"
-    }, {})
+    grove_nursery_recycling_centre = {"id": "way/488949555"}
+    kirkby_moor_wind_farm = {"id": "relation/2928690"}
+    response = handler.hello(grove_nursery_recycling_centre, {})
     payload_json = response['body']
     print(payload_json)
-#    with open("test.png", "wb") as out:
-#        out.write(payload_json)
-#    assert 0 < len(json.loads(payload_json)['features'])
     assert "0 == 1"
 
 test_wind()
